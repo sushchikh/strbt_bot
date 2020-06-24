d34dbbf48250955b062ecaa41e5a444e5cb2eed7
sushchikh <a.sushchikh@gmail.com>
2020-06-24 12:59:03 +0300

    add dowloader, af file exist - download it if not - do not. And in bot if today file exist - use it, else use yesterday

--- py_raw/data_downloader.py
+++ py_raw/data_downloader.py
@@ -9,12 +9,14 @@ def data_downloader():
     # print(path_to_data_file_on_server)
     try:
         data_file = requests.get(path_to_data_file_on_server, allow_redirects=True)
+        if str(data_file.text[:100]).startswith('<!DOCTYPE html>'):  # если вместо файла с датой лезет 404 страница
+            print('Нет датника с сегодняшим числом, 404 ошибка')
+            return
         name_of_saving_file = '/home/sushchikh/strbt_bot/data/' + str(today.strftime("%Y%m%d") + '.csv')
         with open(name_of_saving_file, 'wb') as file:
             file.write(data_file.content)
     except Exception as e:
         print('не нашел свежего датника!')
-    print('data download well done')
 
 
 if __name__ == '__main__':
