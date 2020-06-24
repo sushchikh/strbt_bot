2c6702872d083ca6dcc12b00541bc4b6f068d6a3
sushchikh <a.sushchikh@gmail.com>
2020-06-24 08:25:16 +0300

    see what wrong in downloader

--- py_raw/data_downloader.py
+++ py_raw/data_downloader.py
@@ -5,8 +5,7 @@ import requests
 def data_downloader():
     today = datetime.today()
     # print(today.strftime("%Y%m%d"))
-    path_to_data_file_on_server = ('http://stroybatinfo.ru//import_1c/for_bot/' +
-                                   str(today.strftime("%Y%m%d")) + '.csv')
+    path_to_data_file_on_server = ('http://stroybatinfo.ru/import_1c/for_bot/' + str(today.strftime("%Y%m%d")) + '.csv')
     # print(path_to_data_file_on_server)
     try:
         data_file = requests.get(path_to_data_file_on_server, allow_redirects=True)
